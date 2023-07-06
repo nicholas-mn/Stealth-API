600fd85cf2925156e0c0b8d22650dd7c565b4804
CosmosDev <cosmos.dev@protonmail.com>
2023-07-06 23:19:53 +0200

    Remove JsonClass annotation from SearchResults

--- core/model/src/main/kotlin/com/cosmos/stealth/core/model/api/SearchResults.kt
+++ core/model/src/main/kotlin/com/cosmos/stealth/core/model/api/SearchResults.kt
@@ -1,14 +1,12 @@
 package com.cosmos.stealth.core.model.api
 
 import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
 
 /**
  * 
  *
  * @param type 
  */
-@JsonClass(generateAdapter = true)
 sealed class SearchResults(
     @Json(name = "type")
     val type: SearchType
