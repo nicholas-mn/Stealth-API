e7398defada1721ab802d22d009cd2529bd693eb
CosmosDev <cosmos.dev@protonmail.com>
2023-07-23 17:32:12 +0200

    Replace source and resolutions with media in Reaction

--- core/model/src/main/kotlin/com/cosmos/stealth/core/model/api/Reaction.kt
+++ core/model/src/main/kotlin/com/cosmos/stealth/core/model/api/Reaction.kt
@@ -7,26 +7,21 @@ import com.squareup.moshi.JsonClass
  * Reaction description
  *
  * @param count 
- * @param source 
+ * @param media
  * @param name 
  * @param description 
- * @param resolutions Other resolutions
  */
 @JsonClass(generateAdapter = true)
 data class Reaction (
     @Json(name = "count")
     val count: Int,
 
-    @Json(name = "source")
-    val source: MediaSource,
+    @Json(name = "media")
+    val media: Media,
 
     @Json(name = "name")
     val name: String? = null,
 
     @Json(name = "description")
-    val description: String? = null,
-
-    /* Other resolutions */
-    @Json(name = "resolutions")
-    val resolutions: List<MediaSource>? = null
+    val description: String? = null
 )
--- services/reddit/src/main/kotlin/com/cosmos/stealth/services/reddit/util/extension/AwardingExt.kt
+++ services/reddit/src/main/kotlin/com/cosmos/stealth/services/reddit/util/extension/AwardingExt.kt
@@ -1,15 +1,27 @@
 package com.cosmos.stealth.services.reddit.util.extension
 
+import com.cosmos.stealth.core.model.api.Media
 import com.cosmos.stealth.core.model.api.MediaSource
 import com.cosmos.stealth.core.model.api.Reaction
+import com.cosmos.stealth.core.network.util.extension.mime
+import com.cosmos.stealth.core.network.util.extension.mimeType
 import com.cosmos.stealth.services.reddit.data.model.Awarding
+import io.ktor.http.ContentType
 
 fun Awarding.toReaction(): Reaction {
-    return Reaction(
-        count,
+    val mime = url.mimeType ?: ContentType.Image.PNG
+
+    val media = Media(
+        mime.mime,
         MediaSource(url),
         null,
-        null,
         resizedIcons.map { it.toMediaSource() }
+    )
+
+    return Reaction(
+        count,
+        media,
+        null,
+        null
     )
 }
