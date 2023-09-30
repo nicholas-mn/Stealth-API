540188e8632f8653438493f7fcfcd5b29627e7bb
CosmosDev <cosmos.dev@protonmail.com>
2023-09-30 16:22:02 +0200

    [services][lemmy] Map user's bio to description without markdown parsing

--- services/lemmy/src/main/kotlin/com/cosmos/stealth/services/lemmy/data/mapper/UserMapper.kt
+++ services/lemmy/src/main/kotlin/com/cosmos/stealth/services/lemmy/data/mapper/UserMapper.kt
@@ -2,14 +2,12 @@ package com.cosmos.stealth.services.lemmy.data.mapper
 
 import com.cosmos.stealth.core.common.data.mapper.Mapper
 import com.cosmos.stealth.core.common.di.DispatchersModule.Qualifier.DEFAULT_DISPATCHER_QUALIFIER
-import com.cosmos.stealth.core.common.util.MarkdownParser
 import com.cosmos.stealth.core.model.api.Service
 import com.cosmos.stealth.core.model.api.ServiceName
 import com.cosmos.stealth.core.model.api.UserInfo
 import com.cosmos.stealth.core.model.api.UserType
 import com.cosmos.stealth.core.network.util.extension.toMedia
 import com.cosmos.stealth.services.lemmy.data.model.PersonView
-import com.cosmos.stealth.services.lemmy.di.LemmyModule.Qualifier.LEMMY_QUALIFIER
 import com.cosmos.stealth.services.lemmy.util.extension.toDateInMillis
 import kotlinx.coroutines.CoroutineDispatcher
 import org.koin.core.annotation.Named
@@ -17,7 +15,6 @@ import org.koin.core.annotation.Single
 
 @Single
 class UserMapper(
-    @Named(LEMMY_QUALIFIER) private val markdownParser: MarkdownParser,
     @Named(DEFAULT_DISPATCHER_QUALIFIER) defaultDispatcher: CoroutineDispatcher
 ) : Mapper<PersonView, Service, UserInfo>(defaultDispatcher) {
 
@@ -31,7 +28,7 @@ class UserMapper(
                 person.published.toDateInMillis() ?: System.currentTimeMillis(),
                 person.avatar?.toMedia(),
                 person.banner?.toMedia(),
-                person.bio?.run { markdownParser.parse(this) },
+                person.bio,
                 null,
                 null,
                 null,
