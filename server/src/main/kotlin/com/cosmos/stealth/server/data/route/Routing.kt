afe0b4db7bdd981caccb661f4314d9d560138ff9
CosmosDev <cosmos.dev@protonmail.com>
2023-10-14 00:43:36 +0200

    [server] Add privacy policy

--- server/src/main/kotlin/com/cosmos/stealth/server/data/route/Routing.kt
+++ server/src/main/kotlin/com/cosmos/stealth/server/data/route/Routing.kt
@@ -3,11 +3,16 @@ package com.cosmos.stealth.server.data.route
 import com.cosmos.stealth.server.data.route.v1.v1
 import com.cosmos.stealth.server.util.swaggerUI
 import io.ktor.server.application.Application
+import io.ktor.server.http.content.staticResources
 import io.ktor.server.routing.routing
 
 fun Application.configureRouting() {
     routing {
         swaggerUI("/", "stealth-api.yaml", "Stealth API")
+
+        staticResources("/", "files")
+        staticResources("/en", "en")
+
         v1()
     }
 }
